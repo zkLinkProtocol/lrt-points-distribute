0bb0246af986cd4265825a0b2d73af16d5948c67
xsteadybcgo <xw4585@gmail.com>
2024-06-13 14:18:56 +0800

    fix: api doc

--- src/positions/positions.dto.ts
+++ src/positions/positions.dto.ts
@@ -16,13 +16,25 @@ export class GetUserPositionsDto extends PagingOptionsDto {
 }
 
 export class UserPositionsDto {
-  @ApiProperty()
+  @ApiProperty({
+    type: String,
+    description: "user address",
+    example: "0xc48F99afe872c2541f530C6c87E3A6427e0C40d5",
+  })
   userAddress: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    type: String,
+    description: "token address",
+    example: "0x8280a4e7D5B3B658ec4580d3Bc30f5e50454F169",
+  })
   tokenAddress: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    type: String,
+    description: "token balance",
+    example: "10000000000000000",
+  })
   balance: string;
 }
 
@@ -43,7 +55,7 @@ export class UserPositionsResponseDto {
 
   @ApiProperty({
     type: UserPositionsDto,
-    description: "user points data",
+    description: "user position list",
     nullable: true,
   })
   public readonly data?: UserPositionsDto[];
